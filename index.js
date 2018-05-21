499d873aed36876fc4621b5437d52171c3a3821a
rainyjune <rainyjune@live.cn>
2018-05-21 11:35:29 +0800

    Add iconfonts

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-window.onload = function() {
+$(function() {
 
-  // Click to play the background audio, for Chrome.
-  document.body.addEventListener('click', function() {
+  var counter = 0;
+
+  $(document.body).on('click', function() {
     bgaudio.play();
-  }, true);
+  });
 
-  var thisTable = document.getElementsByTagName('table')[0];
-  thisTable.addEventListener('click', function(e) {
-    e.stopPropagation()
-    var target = e.target;
-    if (target.tagName.toLowerCase() === 'td') {
-      if (target.className.indexOf("todo") > -1) {
-        target.className = "opened tooth";
-        playAudio('audio/yeah.mp3');
-      } else if (target.className === "") {
-        playAudio('audio/no.mp3');
-      }
+  $('table').on('click', 'td', function() {
+    var thisElement = $(this);
+    if (counter === 4){
+      showDots();
+      counter++;
+      return false;
+    } else if (counter === 5) {
+      showHorizonLine();
+      return false;
+    }
+    if (!this.className) {
+      playAudio('audio/no.mp3');
+    } else if (thisElement.hasClass('todo')) {
+      counter++;
+      thisElement.removeClass('todo').addClass("opened tooth");
+      playAudio('audio/yeah.mp3');
     }
   });
 
@@ -23,4 +29,13 @@ window.onload = function() {
     var audio = new Audio(url);
     audio.play();
   }
-};
+
+  function showDots() {
+    $('i.hide').removeClass('hide');
+  }
+
+  function showHorizonLine() {
+    $('table').addClass('done');
+  }
+
+});
